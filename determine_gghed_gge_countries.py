74239ad0672d91179738411a96e7efeeeaddb8a3
Krobix <brendon.shaw8921@gmail.com>
2024-05-26 16:41:44 -0400

    self explanatory

---
+++ determine_gghed_gge_countries.py
@@ -0,0 +1,12 @@
+#Determine which countries in GHED_data.csv have gghed_gge data
+import csv
+
+with open("GHED_data.csv", "r") as f:
+    reader = csv.DictReader(f)
+
+    for i in reader:
+        gghed_gge = i["gghed_gge"]
+        gghed_gge = gghed_gge.strip()
+
+        if not (gghed_gge==""):
+            print(f"{i["country"]}, {i["year"]}")
